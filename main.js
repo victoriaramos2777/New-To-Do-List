7238c4acc36b5f8ddda7bdaedbf4e86d260c6b57
Victoria Ramos <777vikiramos22@gmail.com>
2022-12-21 12:10:37 -0600

    constatntes iniciales creadas

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 //importar de nuestro modulos
 import Items from "./modules/add-remove.js";
 import ToDoItem from "./modules/item-constructor.js";
+
+//constantes iniciales  
 const main = document.getElementById('todo-item');
+const form = document.getElementById('form');
+
+const inputDescription = document.getElementById('todo-item');
+
+const itemsContainer =document.getElementById('todo-cont');
+
+
